47fdc19614b60ebc4c9bd67b47c186ebaf2a4143
Himanshu A <himanshu.aggarwald@gmail.com>
2021-03-14 17:38:33 +0530

    Commiting code for adding up React Project for Assignment of Black Piano in Main Repo

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
-import React, { useState,useEffect } from 'react'
-import Select from 'react-select';
+import React, {useState,useEffect} from 'react'
 import './App.css';
 import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from "react-bootstrap-table2-paginator";
+
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 // functional component named - 'App'
 const App = () => {
-    // setting up hooks for filmData
+    // setting up hooks for filmData and initialize it with blank array
     const [filmData,setFilmData] = useState([]);
-    // on useEffect(on page load) hit the api for films data and populate dropdown based upon it
+    // on useEffect(on page load) hit the api for films data and populate table based upon it using hooks
     useEffect(()=>{
       const apiUrl = 'https://ghibliapi.herokuapp.com/films';
       axios.get(apiUrl)
       .then(res => {
-        // initialize dropdown options with blank array
-        let drpOptions = [];
-        // loop through API reponse and populate dropdown in key/value pair object based upon 'title' of film
-        res.data.forEach((val,i,arr)=>{
-          drpOptions.push({
-            value: val.title,
-            label: val.title
-          });
-        })
-        setFilmData(drpOptions);
+        setFilmData(res.data);
       })
     },[]);
 
-    // onchange method for dropdown
-    const onSelectDrpDown = (drpVal) => {
-      console.log(">>>",drpVal);
-    }
-    // onChange={(e)=>onSelectDrpDown(e.target.value)}
-    const handleSearch = () => {
-      console.log("handling search...");
-    }
+    // setting up columns for table
+    const columns = [{
+      dataField: 'title',
+      text: 'Title'
+    }, {
+      dataField: 'director',
+      text: 'Directed By'
+    }, {
+      dataField: 'release_date',
+      text: 'Release On'
+    }];
     return (
       <div className="container">
-        <h2>A Normal Search Using Dropdown</h2>
-        <form onSubmit={(e)=>e.preventDefault()}>
-          <Select options={filmData} isClearable={true} className="selectDrpDown" classNamePrefix="selectDrpDown"
-          onChange={(e)=>onSelectDrpDown(e.target.value)} value=""/>
-          <button onClick={(e)=>handleSearch()}>Search</button>
-        </form>
-        
+        <h2>A Normal data Fetching into table Using Rest APIs</h2>
+        <BootstrapTable sort={true} bordered={true} keyField='title' data={filmData} columns={columns} 
+        pagination={paginationFactory({sizePerPage:5,hideSizePerPage:true})}/>
       </div>
     );
 }
